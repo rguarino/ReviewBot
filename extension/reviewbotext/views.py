d955dc9f0786ac2abc11b1dd5f4e0604be54e10e
Ross Guarino <rssguar@gmail.com>
2013-07-30 16:08:20 -0700

    Updated the main logging page to link to the runs.
    Also added more links between the run details page and the status
    details page.

--- extension/reviewbotext/views.py
+++ extension/reviewbotext/views.py
@@ -26,9 +26,15 @@ def logging_dashboard(request, template_name='logging/list.html'):
             tools.append("%s.%s" % (tool.tool.entry_point, tool.tool.version))
             print 'Entry Point = [%s.%s]' % (tool.tool.entry_point, tool.tool.version)
         run_info['tools'] = tools
+        run_info['id'] = run.id
         runs_info.append(run_info)
 
-    return render_to_response(template_name, {'workers': worker_names, 'runs': runs_info})
+    from reviewbotext.extension import ReviewBotExtension
+    extension_manager = get_extension_manager()
+    extension = \
+        extension_manager.get_enabled_extension(ReviewBotExtension.id)
+    return render_to_response(template_name, {'workers': worker_names, 'runs': runs_info,\
+        'url': extension._rb_url()})
 
 
 def run_status_details(request, run_id=1, template_name='logging/run_status.html'):
@@ -46,10 +52,32 @@ def run_status_details(request, run_id=1, template_name='logging/run_status.html
         tool_info = {}
         tool_info['name'] = "%s.%s" % (tool.tool.entry_point, tool.tool.version)
         tool_info['status'] = tool.get_status_display()
+        tool_info['id'] = tool.id
         tools.append(tool_info)
     return render_to_response(template_name, {'run': run, 'info': info, 'tools': tools})
 
 
 def tool_status_details(request, status_id=1, template_name='logging/tool_status.html'):
     status = get_object_or_404(ToolStatus, id=status_id)
-    return render_to_response(template_name, {'status': status})
+    run = status.run
+    from reviewbotext.extension import ReviewBotExtension
+    extension_manager = get_extension_manager()
+    extension = \
+        extension_manager.get_enabled_extension(ReviewBotExtension.id)
+    info = {}
+    info['root_url'] = extension._rb_url()
+    info['name'] = "%s.%s" % (status.tool.entry_point, status.tool.version)
+    info['status'] = status.get_status_display()
+    times = []
+    if( status.queued_time != None):
+        times.append(('Queued', status.queued_time))
+    if( status.running_time != None):
+        times.append(('Running', status.running_time))
+    if( status.suceeded_time != None):
+        times.append(('Suceeded', status.running_time))
+    if( status.failed_time != None):
+        times.append(('Failed', status.failed_time))
+    if( status.time_out_time != None):
+        times.append(('Timeout', status.time_out_time))
+
+    return render_to_response(template_name, {'status': status, 'info': info, 'times': times})
